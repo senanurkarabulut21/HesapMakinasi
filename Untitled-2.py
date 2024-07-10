3eb2815b15e1f651ddee877f0781cf5f0535fc37
senanurkarabulut21 <karabulutsenanur24@gmail.com>
2024-07-11 00:34:58 +0300

    HesapMakinasi

---
+++ Untitled-2.py
@@ -0,0 +1,43 @@
+import os
+def topla (a,b):
+   return a+b
+def cikar (a,b):
+   return a-b
+def carp (a,b):
+   return a*b
+def bol (a,b):
+   if 0==int(b):
+     print("Sıfıra bölünme hatası")
+     return 0
+   else:
+      return a/b
+def usal (a,b):
+  return a**b
+while True:
+ os.system('cls')
+ print("""
+ *** HESAP MAKİNESİ ***
+      1- TOPLAMA
+      2- ÇIKARMA
+      3- ÇARPMA
+      4- BÖLME
+      5- ÜS ALMA
+
+  """)
+ sec=input("SEÇİNİZ:")
+ say_a=input("1. Sayı:")
+ say_b=input("2. Sayı:")
+
+ if (sec=="1"):
+   print (topla(int(say_a),int(say_b)))
+ elif (sec=="2"):
+   print (cikar(int(say_a),int(say_b)))
+ elif (sec=="3"):
+   print (carp(int(say_a),int(say_b)))
+ elif (sec=="4"):
+   print (bol(int(say_a),int(say_b)))
+ elif (sec=="5"):
+   print (usal(int(say_a),int(say_b)))
+ else :
+    print ("Geçersiz İşlem")
+ input("Devam etmek için ENTER...")
